19bbaf5a703ffa2771e3550bbc34266c968d9d6a
Harman arora <arora29harman@gmail.com>
2023-09-17 17:09:50 +0530

    changes

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -3,10 +3,11 @@ import './about.css';
 
 export const About = () => {
   return (
-  <div>
+  <div className='abouttext'>
     <h1>
-    Hey, WeLcome to Parkitup 
+    Hey, Welcome to Parkitup 
     </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero omnis explicabo, voluptates quam, esse neque soluta corrupti doloribus voluptatibus laborum enim aspernatur ipsam? Id at doloremque ratione sapiente officiis aliquamtus iusto unde suscipit porro. Laborum, enim explicabo tenetur accusantium autem provident eaque porro libero inventore. Exercitationem, soluta voluptas? Exercitationem officiis nisi dicta, ipsa quasi tempore laudantium amet nobis quia? Quia dicta rem iusto consectetur, voluptas fuga et quisquam repellendus.</p>
   </div>
   )
 }
