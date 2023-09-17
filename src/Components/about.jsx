11179389542e0b858e9f87af2075d7fb804237c0
Harman arora <arora29harman@gmail.com>
2023-09-17 17:14:56 +0530

    Update about.jsx

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -7,7 +7,7 @@ export const About = () => {
     <h1>
     Hey, Welcome to Parkitup 
     </h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero omnis explicabo, voluptates quam, esse neque soluta corrupti doloribus voluptatibus laborum enim aspernatur ipsam? Id at doloremque ratione sapiente officiis aliquamtus iusto unde suscipit porro. Laborum, enim explicabo tenetur accusantium autem provident eaque porro libero inventore. Exercitationem, soluta voluptas? Exercitationem officiis nisi dicta, ipsa quasi tempore laudantium amet nobis quia? Quia dicta rem iusto consectetur, voluptas fuga et quisquam repellendus.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero omnis explicabo, voluptates quam, esse neque soluta corrupti doloribus voluptatibus laborum enim aspernatur ipsam? Id at doloremque ratione sapiente officiis aliquamtus iusto unde suscipit porro. Laborum, enim explicabo tenetur accusantium autem provident eaque porroventore. Exercitationem, soluta voluptas? Exercitationem officiis nisi dicta, ipsa quasi tempore laudantium amet nobis quia? Quia dicta rem iusto consectetur, voluptas fuga et quisquam repellendus.</p>
   </div>
   )
 }
