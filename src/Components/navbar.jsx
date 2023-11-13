12fa7627408b0f4df2021aeb7d1d96f61780feb0
Harman arora <arora29harman@gmail.com>
2023-11-13 22:59:38 +0530

    updates
    updates

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="maincont">
-      <div className="logo">
-        <img src={logo} alt="logo" />
+      <div className="topcontnav">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div
+          className="menu"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-      <div className="links">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/booking">Book Now</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink className="serverlink" to="/server">Server</NavLink>
+      <div
+        className="links"
+        id={menuOpen ? "open" : ""}
+        onClick={() => {
+          setMenuOpen(!menuOpen);
+        }}
+      >
+        <NavLink className="navlink" to="/">
+          Home
+        </NavLink>
+        <NavLink className="navlink" to="/booking">
+          Book Now
+        </NavLink>
+        <NavLink className="navlink" to="/about">
+          About
+        </NavLink>
+        <NavLink className="serverlink" to="/server">
+          Server
+        </NavLink>
       </div>
       {/* <div className="menu-icon">
         <svg
@@ -24,7 +51,6 @@ export const Navbar = () => {
           <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
         </svg>
       </div> */}
-      
     </div>
   );
 };
